5fdec12c3be856d1d182d550eeb4d5d5affff3c4
Holger Hinzberg <holger@hinzberg.de>
2026-02-02 17:02:51 +0100

    Initial Commit

---
+++ JustNotNowDesktop/JustNotNowDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNotNowDesktopApp.swift
+//  JustNotNowDesktop
+//
+//  Created by Holger Hinzberg on 02.02.26.
+//
+
+import SwiftUI
+
+@main
+struct JustNotNowDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
